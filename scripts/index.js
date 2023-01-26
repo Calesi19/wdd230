1fbf1566a8ba36ff4ac99279c5942146f66d9d5b
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-01-26 12:53:19 -0700

    completed portal page

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
-document.getElementById("footer-date").innerHTML("Hello World");
+let date = new Date()
+
+document.getElementById("footer-date").innerHTML = "Last Updated: " + date.toISOString().split('T')[0]
+
+document.getElementById("footer-signature").innerHTML = "&copy; " + date.getFullYear() + " .:|:. Carlos Lespin .:|:. Idaho" 
 
 

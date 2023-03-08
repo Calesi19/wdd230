953a760350e653a31408a6da6567c4278b10717a
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-03-08 13:12:53 -0700

    update

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,9 +1,6 @@
 
 const date = new Date();
 
-
-
-
 document.getElementById("footer-copyright").innerHTML = "&copy; " + date.getFullYear() + " Mountain View Chamber"
 document.getElementById("last-modified").innerHTML = "Last Modified: " + date.toUTCString()
 document.getElementById("current-date").innerHTML = date.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day: "numeric"})
